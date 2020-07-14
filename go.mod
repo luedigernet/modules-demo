dcb98bbbe7e450d45b82b16a19703faf9a50e487
Reinhard Luediger <reinhard.luediger@metronom.com>
2020-07-14 09:37:42 +0200

    Added lokal package
    Signed-off-by: Reinhard Luediger <reinhard.luediger@metronom.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/luedigernet/modules-demo
 
 go 1.13
 
-require github.com/luedigernet/moduleDemo/v2 v2.0.0
+require (
+	github.com/luedigernet/moduleDemo v1.0.1
+	github.com/luedigernet/moduleDemo/v2 v2.0.0
+)
--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
-	demo "github.com/luedigernet/moduleDemo/v2"
+	demo "github.com/luedigernet/moduleDemo"
+	demo2 "github.com/luedigernet/moduleDemo/v2"
+	demo3 "github.com/luedigernet/modules-demo/pkg/demo"
 )
 
 
 
 func main() {
-	result:= demo.SayHello("Reinhard")
+	result:= demo2.SayHello("Reinhard")
 	fmt.Println(result)
+	demo.SayHello()
+	demo3.Add(1,2)
 }
